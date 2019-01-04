101d37e60fb1afb8e308c459bdcd9feda596e067
Danny Schønning <dannys@proactive.dk>
2019-01-04 10:11:17 +0100

    Updated generator

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,79 +1,75 @@
 'use strict';
-var yeoman = require('yeoman-generator'),
-    yosay = require('yosay'),        
-    chalk = require('chalk'),
-    optionOrPrompt = require('yeoman-option-or-prompt'),
-    folderName;
+const Generator = require('yeoman-generator');
+const chalk = require('chalk');
+const yosay = require('yosay');
 
-module.exports = yeoman.Base.extend({
-    _optionOrPrompt: optionOrPrompt,
-	prompting: function() {
-        this.log(yosay('Welcome to the '+chalk.bgRed.white('SharePoint Classic Extension') + ' project generator'));
-		var done = this.async();
+module.exports = class extends Generator {
+    prompting() {
         var config = this.config.getAll();
-		this._optionOrPrompt([
-			{
-				type: 'input',
-				name: 'client',
-				message: 'Customer name',
-				default: config.customer || 'Client',
-                store: !config.customer
-			},
-			{
-				type: 'input',
-				name: 'project',
-				message: 'Project name (example: MyDemoProject)',
-				default: config.project
-			},
-			{
-				type: 'input',
-				name: 'description',
-				message: 'Description',
-                default: config.description
-			}
-		], function(answers) {
-			this.props = answers;
-            for (var key in this.props) {
-                if (this.props.hasOwnProperty(key)) {
-                    var element = this.props[key];
-                    this.config.set(key, element);
-                }
-            }
-			done();
-		}.bind(this));
-    },
 
-	writing: function() {
-        folderName = this.props.client.replace(/ /g, '') + "." + this.props.project.replace(/ /g, '');
-        
+        this.log(
+            yosay(`Welcome to the excellent ${chalk.red('generator-sp-classic')} generator!`)
+        );
+
+        const prompts = [
+            {
+                type: 'input',
+                name: 'client',
+                message: 'Customer name',
+                default: config.customer || 'Client',
+                store: !config.customer
+            },
+            {
+                type: 'input',
+                name: 'project',
+                message: 'Project name (example: MyDemoProject)',
+                default: config.project
+            },
+            {
+                type: 'input',
+                name: 'description',
+                message: 'Description',
+                default: config.description
+            }
+        ];
+
+        return this.prompt(prompts).then(props => {
+            // To access props later use this.props.someAnswer;
+            this.props = props;
+        });
+    }
+
+    writing() {
+        let folderName = this.props.client.replace(/ /g, '') + "-" + this.props.project.replace(/ /g, '');
+
         this.destinationRoot(folderName);
 
         // set template options
         var options = {
-                client: this.props.client, 
-                project: this.props.project,
-                clientLower: this.props.client.toLowerCase(), 
-                projectLower: this.props.project.toLowerCase(),
-                description: this.props.description
-			}
-                    
-        // copy helper functions
-        this.copyRaw = function(folder) {
-            this.fs.copy(
-			     this.templatePath(folder),
-			     this.destinationPath(folder));
+            client: this.props.client,
+            project: this.props.project,
+            clientLower: this.props.client.toLowerCase(),
+            projectLower: this.props.project.toLowerCase(),
+            description: this.props.description
         }
-        this.copyTemplate = function(source) {
+
+        // copy helper functions
+        this.copyRaw = function (folder) {
+            this.fs.copy(
+                this.templatePath(folder),
+                this.destinationPath(folder));
+        }
+        this.copyTemplate = function (source) {
             var target = source
-            .replace(/___ClientProject___/g, (options.client + options.project))
-            .replace(/___Project___/g, options.project)
-            .replace(/^_/g, '')
-            .replace(/\/_/g, '/');
-            
+                .replace(/___ClientProject___/g, (options.client + options.project))
+                .replace(/___Project___/g, options.project)
+                .replace(/^_/g, '')
+                .replace(/\/_/g, '/');
+
             this.fs.copyTpl(
                 this.templatePath(source),
                 this.destinationPath(target),
-                options);            
+                options);
         };
 
         // src files        
@@ -92,47 +88,40 @@ module.exports = yeoman.Base.extend({
         this.copyTemplate("src/___ClientProject___/Services/___mocks__/___Project___Service.ts");
         this.copyTemplate("src/___ClientProject___/Services/___Project___Service.ts");
         this.copyTemplate("src/___ClientProject___/Stores/StateStor.ts");
-        
+
         // - Config
         this.copyTemplate('_config/process-coverage-report.js');
-        
+
         // - Config/Jest
         this.copyTemplate('_config/_Jest/_jest.config.json');
         this.copyTemplate('_config/_Jest/jest.setup.ts');
         this.copyTemplate('_config/_Jest/jest.trx.processor.js');
         // - Config/Jest/Mocks
         this.copyTemplate('_config/_Jest/mocks/_global.mock.ts');
-        
+
         // - Config/WebPack
         this.copyTemplate('_config/_WebPack/_webpack.config.js');
         this.copyTemplate('_config/_WebPack/webpack.common.config.js');
         this.copyTemplate('_config/_WebPack/webpack.dev.server.config.js');
-        
+
         // - Package
-        this.copyTemplate('_package/010_Style_Library/_Deploy.ps1'); 
-        this.copyTemplate('_package/020_Link/_Deploy.ps1'); 
+        this.copyTemplate('_package/010_Style_Library/_Deploy.ps1');
+        this.copyTemplate('_package/020_Link/_Deploy.ps1');
         this.copyTemplate('_package/Install.ps1');
-        
+
         // - Root files
         this.copyRaw(".vscode");
         this.copyTemplate('_Default.html');
         this.copyTemplate('_package.json');
-        this.copyTemplate('_readme.md');        
+        this.copyTemplate('_readme.md');
         this.copyTemplate('_.gitignore');
         this.copyTemplate('_api-server.js');
         this.copyTemplate('_tsconfig.json');
         this.copyTemplate('_tslint.json');
-        
-	},
-    install: function() {
-        if (!this.options.skipInstall) {
-            var that = this;
-            this.npmInstall(undefined,undefined,function(){
-                that.spawnCommandSync('code', ['.']);
-            });
-        }
-    },
-    end: function() {
-        this.log(yosay('Happy coding!!'));
     }
-});
+
+    install() {
+        this.npmInstall();
+        // this.installDependencies();
+    }
+};
